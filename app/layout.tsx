5a2ef57cdaa7d376d88ef5fea7bcce6a6aa8ac96
KITE <108491263+whoskite@users.noreply.github.com>
2025-06-04 02:03:37 -0700

    Remove online font dependency and fix Image component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,9 @@
 import { Navbar } from "@/components/Navbar"
 import { Footer } from "@/components/Footer"
 import type { Metadata } from "next"
-import { Inter } from "next/font/google"
 import "./globals.css"
 import type React from "react" // Added import for React
 import { ThemeProvider } from "@/components/ThemeProvider"
-
-const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "SnapRename: Bulk Image Wizard",
@@ -21,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
-      <body className={`${inter.className} flex flex-col min-h-screen`}>
+      <body className="flex flex-col min-h-screen">
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
